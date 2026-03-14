d34e3e3aa133346be4f6e819d157d0f85374669a
sjsreenathofficial-gif <sj.sreenath.official@gmail.com>
2026-03-14 11:30:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 
 // Firebase Admin
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
