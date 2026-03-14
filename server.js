b2d1755cf3b46c2b8062daf273af06dd9858a7df
sjsreenathofficial-gif <sj.sreenath.official@gmail.com>
2026-03-14 11:34:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const morgan = require('morgan');
 const admin = require('firebase-admin');
 const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
+// Fix private key formatting
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
